0b577fd5e438e82f0172a2c9a6cf20a6353816ac
halezmarie <teacherhaleymarie@gmail.com>
2021-08-10 18:30:16 -0500

    commit tree 32b43b66ef9a57fbad07099a1026506d96463b10

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hour = parseInt(time, 10); //  parseInt() function parses a string and returns an integer
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon" // because 1200-1700 is considered afternoon
+}
+
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
+}
